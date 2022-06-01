55ece68d5d231a5694e307c5098da18bd8705c9a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-02 00:10:43 +0700

    first commit

---
+++ src/main/java/com/irdaislakhuafa/springsecuritygraphql/SpringSecurityGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.springsecuritygraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irdaislakhuafa/springsecuritygraphql/SpringSecurityGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.springsecuritygraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
