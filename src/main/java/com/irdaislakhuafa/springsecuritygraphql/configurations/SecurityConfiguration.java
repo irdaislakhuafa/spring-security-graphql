c94fef4f7700897641fd874df56a199e2da3ae56
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-02 03:06:05 +0700

    enable all endpoint

--- src/main/java/com/irdaislakhuafa/springsecuritygraphql/configurations/SecurityConfiguration.java
+++ src/main/java/com/irdaislakhuafa/springsecuritygraphql/configurations/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.irdaislakhuafa.springsecuritygraphql.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +12,14 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable();
+                // disable login form
+                .csrf().disable()
+
+                .authorizeRequests().anyRequest().permitAll();
+
+        // disable session
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
         // .authorizeRequests().antMatchers("/graphql", "/graphiql").permitAll();
         return http.build();
     }
