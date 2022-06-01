063fb7bf5690114cb80b3401fae67277879317a2
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-02 00:28:13 +0700

    initialize resolvers for Query and Mutation

---
+++ src/main/java/com/irdaislakhuafa/springsecuritygraphql/resolvers/gql/GraphQLMutation.java
@@ -0,0 +1,10 @@
+package com.irdaislakhuafa.springsecuritygraphql.controllers.gql;
+
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@SchemaMapping(typeName = "Mutation")
+public class GraphQLMutation {
+
+}
