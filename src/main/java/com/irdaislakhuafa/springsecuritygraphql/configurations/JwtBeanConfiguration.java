b077f79f733fd75ec779d2ed811936e2920686ac
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-02 04:49:21 +0700

    use io.jsonwebtoken instead on com.auth2

--- src/main/java/com/irdaislakhuafa/springsecuritygraphql/configurations/JwtBeanConfiguration.java
+++ src/main/java/com/irdaislakhuafa/springsecuritygraphql/configurations/JwtBeanConfiguration.java
@@ -1,9 +1,5 @@
 package com.irdaislakhuafa.springsecuritygraphql.configurations;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,19 +14,20 @@ public class JwtBeanConfiguration {
     @Value(value = "${jwt.token.expired.in.minute}")
     private long tokenExpiredInMinute;
 
-    @Bean
-    public Algorithm algorithm() {
-        return Algorithm.HMAC256(this.secretKey);
-    }
+    // @Bean
+    // public Algorithm algorithm() {
+    // return Algorithm.HMAC256(this.secretKey);
+    // }
 
-    @Bean
-    public JWTVerifier jwtVerifier(Algorithm algorithm) {
-        return JWT
-                .require(algorithm)
-                .acceptIssuedAt(System.currentTimeMillis())
-                .acceptExpiresAt(System.currentTimeMillis() + (1000L * 60 * tokenExpiredInMinute))
-                .build();
-    }
+    // @Bean
+    // public JWTVerifier jwtVerifier(Algorithm algorithm) {
+    // return JWT
+    // .require(algorithm)
+    // .acceptIssuedAt(System.currentTimeMillis())
+    // .acceptExpiresAt(System.currentTimeMillis() + (1000L * 60 *
+    // tokenExpiredInMinute))
+    // .build();
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
